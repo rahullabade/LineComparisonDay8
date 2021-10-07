4fea70a0afae79446962e9584d9113c8334a264d
RAHUL LABADE <rahullabade05@gmail.com>
2021-10-07 14:03:27 +0530

    Added: Welcome Message

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.bl.lineComparison;
+
+public class Main {
+	public static void main(String[] args) {
+	Message information = new Message();
+	information.message();
+	}
+
+}
---
+++ Message.java
@@ -0,0 +1,8 @@
+package com.bl.lineComparison;
+
+public class Message {
+	public void message() {
+		System.out.println("Welcome to Line Comparison Computation");
+	}
+
+}
